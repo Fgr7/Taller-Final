5a2ca36b3cacce9cac4a1f3847cdc805b1d22c55
juan <juanthefgr@gmail.com>
2020-04-03 08:13:43 -0500

    Movimiento+obtaculo

---
+++ ConstructorTerran.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ConstructorTerran here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ConstructorTerran extends Terran
+{
+    /**
+     * Act - do whatever the ConstructorTerran wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.isKeyDown("right")){
+        if(getX()<800){
+            setLocation(getX()+1,getY());
+        }
+        }
+        if(Greenfoot.isKeyDown("left")){
+        if(getX()<800){
+            setLocation(getX()-1,getY());
+        }
+        }
+        if(Greenfoot.isKeyDown("up")){
+        if(getY()<600){
+            setLocation(getX(),getY()-1);
+        }
+        }
+        if(Greenfoot.isKeyDown("down")){
+            if(getY()<600){
+        setLocation(getX(),getY()+1);}
+        }
+    }    
+}
---
+++ MedicTerran.java
@@ -0,0 +1,76 @@
+ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MedicTerran here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class MedicTerran extends Terran
+{
+    /**
+     * Act - do whatever the MedicTerran wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+   
+    
+    public void act() 
+    {
+        //movimiento del personaje
+        if(Greenfoot.isKeyDown("right")){
+        if(getX()<900){
+            setLocation(getX()+1,getY());
+        }
+        }
+        if(Greenfoot.isKeyDown("left")){
+        if(getX()<900){
+            setLocation(getX()-1,getY());
+        }
+        }
+        if(Greenfoot.isKeyDown("up")){
+        if(getY()<1000){
+            setLocation(getX(),getY()-1);
+        }
+        }
+        if(Greenfoot.isKeyDown("down")){
+            if(getY()<1000){
+        setLocation(getX(),getY()+1);}
+        }
+        //encuentro con objeto
+        if(isTouching(Arbol.class))
+        {
+           if(getX()<900){
+            setLocation(getX(),getY());
+        }
+        if(Greenfoot.isKeyDown("left")){
+        if(getX()<900){
+            setLocation(getX()+1,getY());
+            
+        }
+        }
+        if(Greenfoot.isKeyDown("up")){
+        if(getY()<1000){
+            setLocation(getX(),getY()+1);
+        }
+        }
+        if(Greenfoot.isKeyDown("down")){
+            if(getY()<1000){
+        setLocation(getX(),getY()-1);}
+        }
+    }
+        //combate
+        if(isTouching(MedicZerg.class)&& Greenfoot.getRandomNumber(10)==3)
+        {
+            
+             getWorld().removeObjects(getWorld().getObjects(MedicTerran.class));
+           
+        }
+        }
+}
+
+
+    
+    
+    
+
